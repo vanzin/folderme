2a85e4fe80b47c0157a3a427f0d75209eae4875e
Marcelo Vanzin <vanzin@apache.org>
2020-10-03 11:45:46 -0700

    Replace socket with dbus integration, add test action.
    App will auto-start when action is triggered. Action is
    currently hardcoded, need to make it more dynamic.

--- DEBIAN/mkdeb.sh
+++ DEBIAN/mkdeb.sh
@@ -10,14 +10,18 @@ mkdir -p $BUILD
 cp -r DEBIAN $BUILD
 cd $BUILD
 
-mkdir -p usr/bin
-mkdir -p usr/share/folderme
 mkdir -p usr/share/applications
-
-cp -r $ROOT/src/* usr/share/folderme
 cp $ROOT/org.vanzin.folderme.desktop usr/share/applications
-(cd usr/bin && ln -sf ../share/folderme/folderme)
+
+mkdir -p usr/share/dbus-1/services
+cp $ROOT/org.vanzin.FolderME.service usr/share/dbus-1/services
+
+mkdir -p usr/share/folderme
+cp -r $ROOT/src/* usr/share/folderme
 rm -rf usr/share/folderme/__pycache__
+
+mkdir -p usr/bin
+(cd usr/bin && ln -sf ../share/folderme/folderme)
 
 cd ..
 dpkg-deb --build folderme
--- src/ipc.py
+++ src/ipc.py
@@ -1,54 +1,39 @@
 # SPDX-License-Identifier: BSD-2-Clause
+import dbus
+import dbus.service
 import os
 import socket
 import threading
 import traceback
 import sys
 import util
+from dbus.mainloop.glib import DBusGMainLoop
 from PyQt5.QtWidgets import QApplication
 
-SOCKET_PATH = "\0" + os.path.join(util.config_dir(), "cmd.socket")
+DBUS_SERVICE = "org.vanzin.FolderME"
+DBUS_OBJECT = "/org/vanzin/FolderME"
+PLAYLIST_IFACE = f"{DBUS_SERVICE}.Playlist"
 
 
-class Server:
+class Server(dbus.service.Object):
     def __init__(self, ui):
+        DBusGMainLoop(set_as_default=True)
+        bus = dbus.SessionBus()
+        bus_name = dbus.service.BusName(DBUS_SERVICE, bus=bus)
+        dbus.service.Object.__init__(self, bus_name, DBUS_OBJECT)
         self.ui = ui
 
-    def serve(self):
-        t = threading.Thread(target=self._serve, daemon=True)
-        t.start()
-
-    def _serve(self):
-        s = socket.socket(socket.AF_UNIX)
-        try:
-            s.bind(SOCKET_PATH)
-            s.listen(1)
-        except:
-            traceback.print_exc()
-            QApplication.instance().exit(1)
-            return
-
-        while True:
-            try:
-                c, _ = s.accept()
-                with c:
-                    cmd = c.recv(1024).decode("utf-8").split("\n")[0]
-                    self._do_command(cmd)
-            except:
-                print("Error in socket receive:", file=sys.stderr)
-                traceback.print_exc()
-
-        s.close()
-
-    def _do_command(self, cmd):
-        if cmd == "quit":
-            self.ui.handleQuit()
+    @dbus.service.method(
+        dbus_interface=PLAYLIST_IFACE, in_signature="", out_signature=""
+    )
+    def playpause(self):
+        if not self.ui.playlist.albums and not self.ui.playlist.is_playing():
+            self.ui.driver.pick_next(play=True)
         else:
-            print(f"Unrecognized command: {cmd}")
+            self.ui.playlist.playpause()
 
 
 def send(cmd):
-    s = socket.socket(socket.AF_UNIX)
-    with s:
-        s.connect(SOCKET_PATH)
-        s.send(cmd.encode("utf-8"))
+    bus = dbus.SessionBus()
+    server = bus.get_object(DBUS_SERVICE, DBUS_OBJECT)
+    server.playpause(dbus_interface=PLAYLIST_IFACE)
