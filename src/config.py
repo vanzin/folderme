1d40fad960d1e9ec4a3997fe70450477d3e72188
Marcelo Vanzin <vanzin@apache.org>
2020-10-11 15:49:52 -0700

    Hook up add/remove buttons in config UI.

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@
 import app
 import browser
 import util
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QDialog, QFileDialog
 
 
 class ConfigDialog(QDialog):
@@ -17,6 +17,18 @@ class ConfigDialog(QDialog):
         self.sbBias.setValue(app.get().bias)
         self.accepted.connect(self._ok)
         self.rejected.connect(self._cancel)
+
+        self.bAdd.clicked.connect(self._add_source)
+        self.bRemove.clicked.connect(self._remove_source)
+
+    def _add_source(self):
+        path = QFileDialog.getExistingDirectory(self)
+        if path:
+            self.lSources.addItem(path)
+
+    def _remove_source(self):
+        for i in self.lSources.selectedItems():
+            r = self.lSources.takeItem(self.lSources.row(i))
 
     def _ok(self):
         sources = [self.lSources.item(x).text() for x in range(self.lSources.count())]
